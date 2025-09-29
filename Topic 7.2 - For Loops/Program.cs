fca85290f4bd6dea6cd5c6f12fc8e02a53d44e0f
Alexander Ronan <339828832@GoTVDSB.ca>
2025-09-29 08:30:51 -0400

    Add project files.

---
+++ Topic 7.2 - For Loops/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_7._2___For_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
